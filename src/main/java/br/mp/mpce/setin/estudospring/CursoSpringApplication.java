898e6dcbcf10a4085ea3ddcec4cdf01c6738ef45
André Rodrigues de Sousa <zenandre@gmail.com>
2018-06-06 12:30:29 -0300

    Projeto Criado

---
+++ src/main/java/br/mp/mpce/setin/estudospring/CursoSpringApplication.java
@@ -0,0 +1,12 @@
+package br.mp.mpce.setin.estudospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+}
